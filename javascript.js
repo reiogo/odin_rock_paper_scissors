33e9d34be585f78e43c0f32659f763c9c8cb199a
reiogo <raogoda2@gmail.com>
2024-06-03 15:53:41 +0900

    Add logic for playing one round.
    In this one I attempted a slightly more efficient method of writing out
    the logic, but it is slightly harder to read I'm afraid.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,72 @@
+// Global scope
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound (humanChoice, computerChoice){
+    let res;
+    let incrementer = NULL;
+    //If choices are equivalent: tie.
+    if (humanChoice === computerChoice) {
+        res = 'tie';
+    }
+    //If choices are scissors and rock, then the rock wins.
+    else if ((computerChoice === 'scissors' || humanChoice === 'scissors') && (computerChoice === 'rock' || humanChoice === 'rock')) {
+        switch (computerChoice) {
+            case 'rock':
+                res = "You lose! Rock beats scissors."
+                incrementer = 'computer'
+                break;
+            case 'scissors':
+                res = "You win! Rock beats scissors."
+                incrementer = 'human'
+                break;
+        }
+    //If choices are paper and scissors, then scissors win.
+    else if ((computerChoice === 'scissors' || humanChoice === 'scissors') && (computerChoice === 'paper' || humanChoice === 'paper')) {
+        switch (computerChoice) {
+            case 'paper':
+                res = "You lose! Paper beats scissors."
+                incrementer = 'computer'
+                break;
+            case 'scissors':
+                res = "You win! Paper beats scissors."
+                incrementer = 'human'
+                break;
+        }
+
+    //If choices are rock and paper, paper wins.
+    else if ((computerChoice === 'rock' || humanChoice === 'rock') && (computerChoice === 'paper' || humanChoice === 'paper')) {
+        switch (computerChoice) {
+            case 'paper':
+                res = "You lose! Paper beats rock."
+                incrementer = 'computer'
+                break;
+            case 'rock':
+                res = "You win! Paper beats rock."
+                incrementer = 'human'
+                break;
+        }
+
+
+    //console log results and increment counter accordingly
+    console.log(res);
+    // switch statement for incrementing whoever won.
+    switch (incrementer) {
+        case 'human':
+            humanScore++;
+            break;
+        case 'computer':
+            computerScore++;
+            break;
+        case NULL:
+            break;
+    }
+    incrementer = NULL;
+
+
+}
+
+
 function getComputerChoice() {
 
     let tmp;
@@ -44,4 +113,3 @@ function getHumanChoice() {
 }
 
 
-console.log (getHumanChoice())
